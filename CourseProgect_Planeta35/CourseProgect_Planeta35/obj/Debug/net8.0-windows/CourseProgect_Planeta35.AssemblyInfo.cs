ac994bdace871d782242a9e42ef81a0b43d8672a
kenikor <romanluev0@gmail.com>
2025-12-02 22:01:34 +0300

    Доделки

--- CourseProgect_Planeta35/CourseProgect_Planeta35/obj/Debug/net8.0-windows/CourseProgect_Planeta35.AssemblyInfo.cs
+++ CourseProgect_Planeta35/CourseProgect_Planeta35/obj/Debug/net8.0-windows/CourseProgect_Planeta35.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseProgect_Planeta35")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b0cc045fab5788da088af63c9d7405448e56fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseProgect_Planeta35")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseProgect_Planeta35")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
